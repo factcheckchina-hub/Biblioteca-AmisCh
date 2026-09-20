9a1640d7eb3732a5486e96766fd7c77071f45062
factcheckchina-hub <factcheckchina@gmail.com>
2026-09-20 20:07:38 +0800

    Add files via upload

---
+++ app/src/main/java/com/factcheckchina/biblioamistadch/MainActivity.java
@@ -0,0 +1,56 @@
+package com.factcheckchina.biblioamistadch;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String HOME_URL = "https://factcheckchina-hub.github.io/Biblioteca-AmisCh/";
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowFileAccess(false);
+        s.setAllowContentAccess(false);
+        s.setBuiltInZoomControls(false);
+        s.setDisplayZoomControls(false);
+        s.setSupportZoom(false);
+        s.setLoadsImagesAutomatically(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Uri uri = request.getUrl();
+                if ("factcheckchina-hub.github.io".equals(uri.getHost())) return false;
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+                return true;
+            }
+            @Override public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Uri uri = Uri.parse(url);
+                if ("factcheckchina-hub.github.io".equals(uri.getHost())) return false;
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+                return true;
+            }
+        });
+        webView.loadUrl(HOME_URL);
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override public void handleOnBackPressed() {
+                if (webView.canGoBack()) webView.goBack(); else finish();
+            }
+        });
+    }
+}
